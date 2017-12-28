78bd89e437429c79bbfc6e9a22b611473ddf1d4f
Aristides Milios <aristides.milios@gmail.com>
2017-12-28 18:52:51 -0400

    initial commit with cli folder structure

---
+++ src/components/Splash.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="splash">
+    <section class="bgimage">
+      <div class="container h-90 d-flex justify-content-center">
+        <div class="jumbotron header-div">
+          <h1 class="display-3 hero-text">{{ $t('splash.mainTitle') }}</h1>
+
+          <a class="language-toggle-btn btn btn-warning rounded-0" @click="toggleLanguage">{{ $t('splash.toggle') }} </a>
+
+          <br />
+            <nav class="nav flex-column flex-sm-row">
+              <a class="nav-link btn btn-outline-info btn-menu-c rounded-0" href="#">{{ $t('splash.commHallRent') }}</a>
+              <a class="nav-link btn btn-outline-info btn-menu-c rounded-0" href="#">{{ $t('splash.bylaws') }}</a>
+              <a class="nav-link btn btn-outline-info btn-menu-c rounded-0" href="#">{{ $t('splash.contactinfo') }}</a>
+              <a class="nav-link btn btn-outline-info btn-menu-c rounded-0" href="#">{{ $t('splash.announcements') }}</a>
+            </nav>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Splash',
+  data() {
+    return {};
+  },
+  methods: {
+    toggleLanguage() {
+      if (this.$i18n.locale === 'gr') {
+        this.$i18n.locale = 'en';
+      } else {
+        this.$i18n.locale = 'gr';
+      }
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .bgimage {
+    width:100%;
+    height:100vh;
+    background: url('../assets/greek_church.jpg');
+    background-repeat: no-repeat;
+    background-position: top;
+    background-size:cover;
+    background-attachment: fixed;
+  }
+
+  .hero-text {
+      font-size: 2em;
+  }
+
+  .header-div {
+      margin-top: 32%;
+      margin-left: 10%;
+      margin-right: 10%;
+  }
+
+  @media (min-width: 576px) {
+      :not(.nav .nav-link:last-child) {
+          border-right: 0;
+      }
+  }
+  @media (max-width: 576px) {
+      :not(.nav .nav-link:last-child) {
+          border-bottom: 0;
+      }
+  }
+
+  .language-toggle-btn {
+      position: absolute;
+      right: 0px;
+      top: 0px;
+      cursor: pointer;
+  }
+
+  .btn-menu-c {
+      border-color: #27ae60;
+      color: #27ae60;
+  }
+
+  .btn-menu-c:hover {
+      background-color: #27ae60;
+      border-color: #27ae60;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,55 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueI18n);
+
+let userLang = navigator.language || navigator.userLanguage;
+userLang = userLang.toLowerCase();
+
+if (userLang.indexOf('gr') !== -1) {
+  userLang = 'gr';
+} else {
+  userLang = 'en';
+}
+
+const resourceStore = {
+  en: {
+    splash: {
+      mainTitle: 'St. George\'s Greek Orthodox Church and Community Centre',
+      commHallRent: 'Community Hall Rental',
+      bylaws: 'Bylaws',
+      contactinfo: 'Contact Info',
+      announcements: 'Misc Announcements',
+      toggle: 'Αλλαγή Γλώσσας',
+    },
+  },
+
+  gr: {
+    splash: {
+      mainTitle: 'Ελληνική Ορθόδοξη Εκκλησία Αγ. Γεωργίου και Κοινοτικό Κέντρο',
+      commHallRent: 'Ενοικίαση Κοινοτικής Αίθουσας',
+      bylaws: 'Εσωτερικοί Κανονισμοί',
+      contactinfo: 'Πληροφορίες Επικοινωνίας',
+      announcements: 'Ανακοινώσεις',
+      toggle: 'Switch to English',
+    },
+  },
+};
+
+const i18n = new VueI18n({
+  locale: userLang, // set locale
+  fallbackLocale: 'en',
+  messages: resourceStore, // set locale messages
+});
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  i18n,
+  el: '#app',
+  render: h => h(App),
+});
